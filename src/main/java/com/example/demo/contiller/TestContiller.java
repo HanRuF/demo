33de83fca6fa386d6808666a808e90e10328e5f1
羽 <2444717083@qq.com>
2019-01-10 10:50:17 +0800

    测试情况复习

--- src/main/java/com/example/demo/contiller/TestContiller.java
+++ src/main/java/com/example/demo/contiller/TestContiller.java
@@ -3,14 +3,22 @@ package com.example.demo.contiller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
-@RestController(value = "/a")
+@RestController
 public class TestContiller {
 
 
-    @RequestMapping(value = "/view",method = RequestMethod.GET)
+    @RequestMapping(value = "/view")
     public  String view(){
+        return "helloword";
+    }
 
-        return "hello word ";
+    @RequestMapping(value = "/index")
+    public ModelAndView index (){
+        ModelAndView mv=new ModelAndView();
+        mv.setViewName("index.html");
+        mv.addObject("name", "liyafeisss");
+        return mv;
     }
 }
